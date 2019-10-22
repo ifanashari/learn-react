f27b53eb5ccdcce5dbf567ebe6a36e894ab416ce
ifan <ifanashari1234@gmail.com>
2019-10-22 14:29:33 +0700

    initialize news portal

---
+++ src/pages/news/newsList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class NewsList extends Component {
+    render() {
+        return (
+            <div>
+                NewsList
+            </div>
+        )
+    }
+}
